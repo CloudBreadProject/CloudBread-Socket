7ce679ac7c70cf2f4e0a03dca1c1c714e23d7e35
beingbook@gmail.com <beingbook@gmail.com>
2016-01-20 13:31:26 +0900

    Add user get route

--- src/api/user/user.js
+++ src/api/user/user.js
@@ -1,5 +1,20 @@
 import { Router } from 'express';
+import User from 'models/User';
 
 const router = Router();
+
+router
+  .route('/users')
+  .get(async (req, res, next) => {
+    try {
+      const users = await User.find();
+      return res.send(users);
+    } catch (error) {
+      return next(error);
+    }
+  });
+
+router
+  .route('/users/:id');
 
 export default router;
